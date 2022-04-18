c0c8e1be2440e1585863ac8f7b4225e978c49f92
Nguyen Tai Tu <nguyentaitupy@gmail.com>
2022-04-18 21:09:26 +0700

    Topic- Java Basic: Eclipse IDE Tips

---
+++ javaTester/ecliseTips/Topic_01_Template.java
@@ -0,0 +1,22 @@
+package ecliseTips;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
+public class Topic_01_Template {
+	static WebDriver driver = null;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebElement registerPgae = driver.findElement(By.xpath(""));
+		
+		WebElement idname = driver.findElement(By.cssSelector(""));
+		
+		Assert.assertTrue(false);
+		
+		Faker fake = new Faker();
+	}
+
+}
