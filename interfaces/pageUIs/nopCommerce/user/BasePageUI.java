adc09d9c98130d931ca2a655218733b7512d0631
Nguyen Tai Tu <nguyentaitupy@gmail.com>
2022-05-17 21:59:39 +0700

    Build Framework - By Locator Refactor/ String Format/ Rest Parameter

--- interfaces/pageUIs/nopCommerce/user/BasePageUI.java
+++ interfaces/pageUIs/nopCommerce/user/BasePageUI.java
@@ -1,10 +1,11 @@
 package pageUIs.nopCommerce.user;
 
 public class BasePageUI {
-	public static final String ADDRESS_LINK = "//div[@class='block block-account-navigation']//a[text()='Addresses']";
-	public static final String CUSTOMER_INFOR_LINK = "//div[@class='block block-account-navigation']//a[text()='Customer info']";
-	public static final String MY_PRODUCT_REVIEW_LINK = "//div[@class='block block-account-navigation']//a[text()='My product reviews']";
-	public static final String REWARD_POINT_LINK = "//div[@class='block block-account-navigation']//a[text()='Reward points']";
-	public static final String LOGOUT_LINK_AT_USER = "//a[@class='ico-logout']";
-	public static final String LOGOUT_LINK_AT_ADMIN = "//a[text()='Logout']";
+	public static final String ADDRESS_LINK = "XPATH=//div[@class='block block-account-navigation']//a[text()='Addresses']";
+	public static final String CUSTOMER_INFOR_LINK = "XPath=//div[@class='block block-account-navigation']//a[text()='Customer info']";
+	public static final String MY_PRODUCT_REVIEW_LINK = "Xpath=//div[@class='block block-account-navigation']//a[text()='My product reviews']";
+	public static final String REWARD_POINT_LINK = "xpath=//div[@class='block block-account-navigation']//a[text()='Reward points']";
+	public static final String LOGOUT_LINK_AT_USER = "css=a[class='ico-logout']";
+	public static final String LOGOUT_LINK_AT_ADMIN = "xpath=//a[text()='Logout']";
+	public static final String LOADING_PAGE_ADMIN = "xpath=//div[@id='ajaxBusy']/span";
 }
--- interfaces/pageUIs/nopCommerce/user/CustomerInforPageUI.java
+++ interfaces/pageUIs/nopCommerce/user/CustomerInforPageUI.java
@@ -1,5 +1,5 @@
 package pageUIs.nopCommerce.user;
 
 public class CustomerInforPageUI {
-	public static final String CUSTOMER_INFOR_HEADER = "//div[@class='page-title']/h1";
+	public static final String CUSTOMER_INFOR_HEADER = "XPath=//div[@class='page-title']/h1";
 }
