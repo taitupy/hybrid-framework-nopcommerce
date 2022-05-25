27d959d1adb84d104a2dbe0b1a323dad18651340
Nguyen Tai Tu <nguyentaitupy@gmail.com>
2022-05-25 22:19:18 +0700

    Java OOP - Part VIII: Polymorphism and Abstraction

---
+++ javaTester/javaOOP/abstraction/Computer.java
@@ -0,0 +1,11 @@
+package javaOOP.abstraction;
+
+public abstract class Computer {
+	// Non-abstract (Normal)
+	public void showCPU() {
+		System.out.println("Intel CPU");
+	}
+	
+	// Abstract
+	public abstract void setRam();
+}
---
+++ javaTester/javaOOP/abstraction/Laptop.java
@@ -0,0 +1,15 @@
+package javaOOP.abstraction;
+
+public class Laptop extends Computer{
+
+	@Override
+	public void setRam() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void renderVideo() {
+		showCPU();
+	}
+
+}
